d6340993af7bb296bb624ec66250b974e0791ae8
Shamseer <shamseertk@gmail.com>
2020-06-14 17:25:59 -0700

    Fixed nslint error and verbiage

--- src/container/Home.js
+++ src/container/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return <React.Fragment>
     <div className="container">
      <h2>Welcome to arabic learning portal!.</h2> 
-     <p>Start from level1 and learn reading Arabic. </p>
+     <p>Start from level1 and learn reading and writing Arabic. </p>
     </div>
   </React.Fragment>
 }
